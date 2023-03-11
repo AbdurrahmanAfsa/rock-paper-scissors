f20cf70c37e04bb45cb64d1910bb1e8c30320946
Abdurrahman Afsa <23615796+AbdurrahmanAfsa@users.noreply.github.com>
2023-03-11 14:29:50 +0200

    Declare playRound function which contains all the possible conditions in the game and prints out the result.

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ function getComputerChoice (){
     let computerChoice = choices[Math.floor(Math.random() * choices.length)];
     return computerChoice;
 }
+
+function playRound (playerSelection , computerSelection) {
+    if (playerSelection == "rock" && computerSelection == "paper"){
+        return "You lose, paper beats rock"
+    }else if (playerSelection == "rock" && computerSelection == "scissors"){
+        return "You win !!"
+    }else if (playerSelection == "paper" && computerSelection == "rock"){
+        return "You win !!"
+    }else if (playerSelection == "paper" && computerSelection == "scissors"){
+        return "You lose! scissors beat paper"
+    }else if (playerSelection == "scissors" && computerSelection == "rock"){
+        return "You lose! rock beats scissors"
+    }else if (playerSelection == "scissors" && computerSelection == "paper"){
+        return "You win !!"
+    }else {
+        return "It's a tie"
+    }
+}
