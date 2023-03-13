9d0e60844910e750a1b0f43277f6c13520dbc9d4
Abdurrahman Afsa <23615796+AbdurrahmanAfsa@users.noreply.github.com>
2023-03-13 14:10:22 +0200

    Commenting out game function.

--- script.js
+++ script.js
@@ -25,36 +25,36 @@ function playRound (playerSelection , computerSelection) {
     return result
 }
 
-function game (){
-    let playerScore = 0;
-    let computerScore = 0;
-    let gameResult = ``
-    for (let i = 0 ; i < 5 ; i++){
-        let playerSelection = prompt("Enter rock ,paper or scissors");
-        let computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection , computerSelection))
-        if (result == "You lose! rock beats scissors"){
-            computerScore += 1;
-        }else if (result == "You lose! scissors beat paper"){
-            computerScore += 1;
-        }else if (result == "You lose! paper beats rock"){
-            computerScore += 1;
-        }else if (result == "You win !!"){
-            playerScore += 1;
-        }else {
-            playerScore = playerScore;
-            computerScore = computerScore;
-        }
-        console.log(`Result: You(${playerScore}) - Computer(${computerScore})`)
-    }
-    if (playerScore > computerScore){
-        gameResult = 'Congratulations, you won !!'
-    }else if (computerScore > playerScore){
-        gameResult = 'You lost, try another time'
-    }else {
-        gameResult = `It's a tie`
-    }
-    console.log(gameResult)
-}
+// function game (){
+//     let playerScore = 0;
+//     let computerScore = 0;
+//     let gameResult = ``
+//     for (let i = 0 ; i < 5 ; i++){
+//         let playerSelection = prompt("Enter rock ,paper or scissors");
+//         let computerSelection = getComputerChoice();
+//         console.log(playRound(playerSelection , computerSelection))
+//         if (result == "You lose! rock beats scissors"){
+//             computerScore += 1;
+//         }else if (result == "You lose! scissors beat paper"){
+//             computerScore += 1;
+//         }else if (result == "You lose! paper beats rock"){
+//             computerScore += 1;
+//         }else if (result == "You win !!"){
+//             playerScore += 1;
+//         }else {
+//             playerScore = playerScore;
+//             computerScore = computerScore;
+//         }
+//         console.log(`Result: You(${playerScore}) - Computer(${computerScore})`)
+//     }
+//     if (playerScore > computerScore){
+//         gameResult = 'Congratulations, you won !!'
+//     }else if (computerScore > playerScore){
+//         gameResult = 'You lost, try another time'
+//     }else {
+//         gameResult = `It's a tie`
+//     }
+//     console.log(gameResult)
+// }
 
-game ();
+// game ();
